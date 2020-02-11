4ed0e7a4e88af4b6b7f9152a769f8af62b734340
maxkorzan <maxkorzan@gmail.com>
2020-02-11 13:24:23 -0500

    first commit

---
+++ src/test/java/com/max/uselessform/UselessformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.uselessform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UselessformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
